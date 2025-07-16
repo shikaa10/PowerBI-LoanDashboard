d6c509e9f24aeffcd4801cfed390fc9f32831cdb
shikaa10 <aashika.bs2022@vitstudent.ac.in>
2025-07-16 21:38:12 +0530

    Create LoanStatus_Analysis.sql

---
+++ LoanStatus_Analysis.sql
@@ -0,0 +1,19 @@
+-- Loan Status Grid
+SELECT 
+    loan_status, 
+    COUNT(id) AS LoanCount, 
+    SUM(total_payment) AS Total_Amount_Received, 
+    SUM(loan_amount) AS Total_Funded_Amount, 
+    AVG(int_rate * 100) AS Interest_Rate, 
+    AVG(dti * 100) AS DTI 
+FROM bank_loan_data 
+GROUP BY loan_status;
+
+-- MTD Loan Status Summary (Month = 12)
+SELECT 
+    loan_status, 
+    SUM(total_payment) AS MTD_Total_Amount_Received, 
+    SUM(loan_amount) AS MTD_Total_Funded_Amount 
+FROM bank_loan_data 
+WHERE MONTH(issue_date) = 12 
+GROUP BY loan_status;
